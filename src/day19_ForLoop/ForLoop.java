a1d24fd8923978f804deba8b0a46d4d0692c8993
“your_github_username” <yusufdgnyznfa@gmail.com>
2019-09-13 16:03:51 -0500

    push

--- src/day19_ForLoop/ForLoop.java
+++ src/day19_ForLoop/ForLoop.java
@@ -2,4 +2,36 @@ package day19_ForLoop;
 
 public class ForLoop {
 
+	public static void main(String[] args) {
+		/*
+		 
+		 for( initialization; condition; iterator ) {
+		 
+		 			statements that we awanna repeat
+		 			
+		 	}
+		 	
+		 	initialization: starting point    int i = 10 ....
+		 	Condition: created based on the initialization   i > 0 ...
+		 	iterator: co-related to the condition.
+		 	 		Needs to be capable enough to make the condition false
+		 	 		
+		 	 if the condition of loop is true, the loop will be keep executed		
+		 	
+		 */
+		
+		for(double i = 1; i <= 10 ; i += 2 ) {
+			System.out.println("Hello World "+i);
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
 }
--- src/day19_ForLoop/Practice.java
+++ src/day19_ForLoop/Practice.java
@@ -2,4 +2,19 @@ package day19_ForLoop;
 
 public class Practice {
 
+public static void main(String[] args) {
+		
+		String str ="ABC";
+		String str2 ="abc";
+		
+		if(  ! str.equals(str2) ) {
+			System.out.println("Equal");
+		} else {
+			System.out.println("not equal");
+		}
+		
+		
+		
+	}
+
 }

d348f20b80b40c50562ec10bfea8b8eb72c9804e
yusufdogan38 <yusufdgnyznfa@gmail.com>
2019-10-28 11:08:31 -0500

    push

--- src/day44_AccessModifiers/Test.java
+++ src/day44_AccessModifiers/Test.java
@@ -1,5 +1,25 @@
 package day44_AccessModifiers;
 
+package day44_AccessModifiers;
+import static day44_AccessModifiers.Testdata.ID;
+import java.util.Arrays;
+//import static day44_AccessModifiers.Testdata.Name;
+//                  packagename    classname.staticName
+ 
+import static day44_AccessModifiers.Testdata.*;
+ 
 public class Test {
-
+    public static String Name ="Muhtar";
+    
+    public static void main(String[] args) {
+        
+        System.out.println( Name );
+        System.out.println(Testdata.Name);
+        System.out.println( ID );
+        System.out.println( SchoolName);
+        
+        
+        
+        
+    }
 }
--- src/day44_AccessModifiers/Testdata.java
+++ src/day44_AccessModifiers/Testdata.java
@@ -1,5 +1,9 @@
 package day44_AccessModifiers;
 
 public class Testdata {
+	public static String Name ="Juline";
+    public static String ID = "007";
+    public static String SchoolName = "Cybertek";
+
 
 }
